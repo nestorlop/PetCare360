d0ea7e56cb8cc2dbeff9bc4c69dca4b4bfc51a8c
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-09-26 09:54:31 -0500

    Maven y demas

---
+++ src/main/java/com/dosw/PetCare360/demo/PetCare360Application.java
@@ -0,0 +1,13 @@
+package com.dosw.PetCare360.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCare360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCare360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dosw/PetCare360/demo/PetCare360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dosw.PetCare360.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCare360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

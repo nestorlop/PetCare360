f72c964605022faad0591bbb79e426d9d66ddd77
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-10-07 14:23:06 -0500

    Carpeta de repositorio.Vacia por que es sin persistencia

--- src/main/java/com/dosw/PetCare360/Model/Pet.java
+++ src/main/java/com/dosw/PetCare360/Model/Pet.java
@@ -1,12 +1,9 @@
 package com.dosw.PetCare360.Model;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-
 @Data
 @Builder
 public class Pet {
-    @Id
     private Long id;
     private String name;
     private String type;
